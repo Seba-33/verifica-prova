fe1ab5e9dbd379d311b870feb5a0cc41e63ba0a7
Seba-33 a <mihali.sebastian@iisgalvanimi.edu.it>
2024-12-04 22:33:44 +0000

    secondo commit

--- Mihali/src/app/primo/primo.component.ts
+++ Mihali/src/app/primo/primo.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { SecondoComponent } from '../secondo/secondo.component';
+
 
 
 @Component({
   selector: 'app-primo',
   standalone: true,
-  imports: [CommonModule,SecondoComponent],
+  imports: [CommonModule],
   templateUrl: './primo.component.html',
   styleUrl: './primo.component.css'
 })
